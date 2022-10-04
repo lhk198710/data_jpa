aa048f0add5a3dc5a07706f0841cd05192338354
lhk871010 <lhk8710@lfcorp.com>
2022-10-04 13:48:37 +0900

    @Query, 값, DTO 조회하기

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import javax.annotation.PreDestroy;
@@ -20,4 +21,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     // 복잡한 쿼리 조회 필요 시 사용
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    // Query를 이용해 내가 생성한 DTO 조회하기
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
